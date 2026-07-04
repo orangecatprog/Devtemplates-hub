fe4d2421b4f67e69e4825fc7295fcd34f277db8c
orangecatprog <luisrguezgam@gmail.com>
2026-07-04 15:24:05 +0200

    fix(template): fix TemplateData Description import

--- src/template/core/domain/entity/TemplateData.ts
+++ src/template/core/domain/entity/TemplateData.ts
@@ -1,13 +1,13 @@
 import type { Url } from "@/shared/core/domain/url/Url";
 import type { Title } from "./title/Title";
-import type { Description } from "./description/Description";
+import type { TemplateDescription } from "./description/Description";
 import type { Difficulty } from "./difficulty/Difficulty";
 import type { UniqueID } from "@/shared/core/domain/uniqueID/UniqueID";
 import type { State } from "./state/State";
 
 export interface TemplateData {
 	title: Title;
-	description: Description;
+	description: TemplateDescription;
 	repositoryUrl: Url;
 	demoUrl: Url | undefined;
 	difficulty: Difficulty;
