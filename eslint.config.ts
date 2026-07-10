f3b82a4d094f4641137264cc446bdc387964613b
orangecatprog <luisrguezgam@gmail.com>
2026-07-10 12:06:53 +0200

    test(user): add tests to role

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,49 +1,20 @@
-import css from "@eslint/css";
 import js from "@eslint/js";
-import json from "@eslint/json";
-import markdown from "@eslint/markdown";
+import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
 import globals from "globals";
-import tseslint from "typescript-eslint";
 
 export default defineConfig([
-	{
-		files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
-		plugins: { js },
-		extends: ["js/recommended", "prettier"],
-		languageOptions: { globals: { ...globals.browser, ...globals.node } },
-	},
+	js.configs.recommended,
+	...tseslint.configs.recommended,
 	eslintConfigPrettier,
-	tseslint.configs.recommended,
 	{
-		files: ["**/*.json"],
-		plugins: { json },
-		language: "json/json",
-		extends: ["json/recommended"],
-	},
-	{
-		files: ["**/*.jsonc"],
-		plugins: { json },
-		language: "json/jsonc",
-		extends: ["json/recommended"],
-	},
-	{
-		files: ["**/*.json5"],
-		plugins: { json },
-		language: "json/json5",
-		extends: ["json/recommended"],
-	},
-	{
-		files: ["**/*.md"],
-		plugins: { markdown },
-		language: "markdown/gfm",
-		extends: ["markdown/recommended"],
-	},
-	{
-		files: ["**/*.css"],
-		plugins: { css },
-		language: "css/css",
-		extends: ["css/recommended"],
+		files: ["**/*.{ts,mts,cts,js,mjs,cjs}"],
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				...globals.node,
+			},
+		},
 	},
 ]);
